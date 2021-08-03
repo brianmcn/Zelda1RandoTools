e40c5aea50d08891aecb6264c5cde83b82453d3d
brianmcn <lorgon@gmail.com>
2021-08-03 10:05:28 -0400

    initial selector screen for Avalonia UI

--- Z1R_Tracker/Z1R_Avalonia/Program.fs
+++ Z1R_Tracker/Z1R_Avalonia/Program.fs
@@ -1,13 +1,77 @@
 ﻿open System
 open Avalonia
 open Avalonia.Controls
+open Avalonia.Media
+open Avalonia.Layout
 
-type MyWindow() as this = 
+type MyCommand(f) =
+    let ev = new Event<EventArgs>()
+    interface System.Windows.Input.ICommand with
+        member this.CanExecute(x) = true
+        member this.Execute(x) = f(x)
+        //[<CLIEventAttribute>]
+        //member this.CanExecuteChanged = ev.Publish
+        member this.add_CanExecuteChanged(h) = ev.Publish.AddHandler(fun o ea -> h.Invoke(o,ea))
+        member this.remove_CanExecuteChanged(h) = ev.Publish.RemoveHandler(fun o ea -> h.Invoke(o,ea))
+
+type MyWindow(owMapNum) as this = 
     inherit Window()
+    let mutable startTime = DateTime.Now
+    let mutable canvas, updateTimeline = null, fun _ -> ()
+    let hmsTimeTextBox = new TextBox(Text="timer",FontSize=42.0,Background=Brushes.Black,Foreground=Brushes.LightGreen,BorderThickness=Thickness(0.0))
+    //                 items  ow map  prog  timeline                     dungeon tabs                
+    let HEIGHT = float(30*4 + 11*3*9 + 30)// TODO + 3*WPFUI.TLH + 3 + WPFUI.TH + 27*8 + 12*7 + 30 + 40) // (what is the final 40?)
+    let WIDTH = float(16*16*3 + 16)  // ow map width (what is the final 16?)
     do
+        // TODO WPFUI.timeTextBox <- hmsTimeTextBox
+        let timer = new Avalonia.Threading.DispatcherTimer()
+        timer.Interval <- TimeSpan.FromSeconds(1.0)
+        timer.Tick.Add(fun _ -> this.Update(false))
+        timer.Start()
+        this.KeyBindings.Add(new Avalonia.Input.KeyBinding(Gesture=Avalonia.Input.KeyGesture.Parse("F5"), Command=new MyCommand(fun x -> 
+                printfn "F5 was pressed"
+                // TODO TrackerModel.startIconX <- WPFUI.currentlyMousedOWX
+                // TODO TrackerModel.startIconY <- WPFUI.currentlyMousedOWY
+                TrackerModel.forceUpdate()
+                )))
+        this.KeyBindings.Add(new Avalonia.Input.KeyBinding(Gesture=Avalonia.Input.KeyGesture.Parse("F10"), Command=new MyCommand(fun x -> 
+                this.Update(true)
+                )))
+
         // full window
         this.Title <- "Zelda 1 Randomizer"
-
+        let stackPanel = new StackPanel(Orientation=Orientation.Vertical)
+        let tb = new TextBox(Text="Choose overworld quest:")
+        stackPanel.Children.Add(tb) |> ignore
+        let owQuest = new ComboBox()
+        owQuest.Items <- [|
+                "First Quest"
+                "Second Quest"
+                "Mixed - First Quest"
+                "Mixed - Second Quest"
+            |]
+        owQuest.SelectedIndex <- owMapNum % 4
+        stackPanel.Children.Add(owQuest) |> ignore
+        let tb = new TextBox(Text="\nNote: once you start, you can use F5 to\nplace the 'start spot' icon at your mouse,\nor F10 to reset the timer to 0, at any time\n",IsReadOnly=true)
+        stackPanel.Children.Add(tb) |> ignore
+        let startButton = new Button(Content=new TextBox(Text="Start Z-Tracker",IsReadOnly=true,IsHitTestVisible=false))
+        stackPanel.Children.Add(startButton) |> ignore
+        let hstackPanel = new StackPanel(Orientation=Orientation.Horizontal, HorizontalAlignment=HorizontalAlignment.Center)
+        hstackPanel.Children.Add(stackPanel) |> ignore
+        this.Content <- hstackPanel
+        startButton.Click.Add(fun _ ->
+                printfn "you pressed start after selecting %d" owQuest.SelectedIndex
+                // TODO stuff
+            )
+    member this.Update(f10Press) =
+        // update time
+        let ts = DateTime.Now - startTime
+        let h,m,s = ts.Hours, ts.Minutes, ts.Seconds
+        hmsTimeTextBox.Text <- sprintf "%02d:%02d:%02d" h m s
+        // update timeline
+        if f10Press || ts.Seconds = 0 then
+            printfn "F10 was pressed"
+            updateTimeline(int ts.TotalMinutes)
 
 type App() =
     inherit Avalonia.Application()
@@ -20,7 +84,7 @@ type App() =
     override this.OnFrameworkInitializationCompleted() =
         match this.ApplicationLifetime with
         | :? Avalonia.Controls.ApplicationLifetimes.IClassicDesktopStyleApplicationLifetime as desktop ->
-            desktop.MainWindow <- new MyWindow()
+            desktop.MainWindow <- new MyWindow(0)
         | _ -> ()
         base.OnFrameworkInitializationCompleted()
 
--- Z1R_Tracker/Z1R_WPF/Program.fs
+++ Z1R_Tracker/Z1R_WPF/Program.fs
@@ -73,15 +73,10 @@ type MyWindowBase() as this =
                     f5WasRecentlyPressed <- true
         IntPtr.Zero
 
-type MyWindow(isHeartShuffle,owMapNum) as this = 
+type MyWindow(owMapNum) as this = 
     inherit MyWindowBase()
     let mutable canvas, updateTimeline = null, fun _ -> ()
     let hmsTimeTextBox = new TextBox(Text="timer",FontSize=42.0,Background=Brushes.Black,Foreground=Brushes.LightGreen,BorderThickness=Thickness(0.0))
-    let mutable ladderTime        = DateTime.Now.Subtract(TimeSpan.FromMinutes(10.0)) // ladderTime        starts in past, so that can instantly work at startup for debug testing
-    let mutable recorderTime      = DateTime.Now.Subtract(TimeSpan.FromMinutes(10.0)) // recorderTime      starts in past, so that can instantly work at startup for debug testing
-    let mutable powerBraceletTime = DateTime.Now.Subtract(TimeSpan.FromMinutes(10.0)) // powerBraceletTime starts in past, so that can instantly work at startup for debug testing
-    let da = new System.Windows.Media.Animation.DoubleAnimation(From=System.Nullable(1.0), To=System.Nullable(0.0), Duration=new Duration(System.TimeSpan.FromSeconds(0.5)), 
-                AutoReverse=true, RepeatBehavior=System.Windows.Media.Animation.RepeatBehavior.Forever)
     //                 items  ow map  prog  timeline                     dungeon tabs                
     let HEIGHT = float(30*4 + 11*3*9 + 30 + 3*WPFUI.TLH + 3 + WPFUI.TH + 27*8 + 12*7 + 30 + 40) // (what is the final 40?)
     let WIDTH = float(16*16*3 + 16)  // ow map width (what is the final 16?)
@@ -240,10 +235,8 @@ let main argv =
             app.Run(TimerOnlyWindow()) |> ignore
         elif argv.Length > 0 && argv.[0] = "terraria" then
             app.Run(TerrariaTimerOnlyWindow()) |> ignore
-        elif argv.Length > 0 && argv.[0] = "heartShuffle" then
-            app.Run(MyWindow(true,owMapNum)) |> ignore
         else
-            app.Run(MyWindow(false,owMapNum)) |> ignore
+            app.Run(MyWindow(owMapNum)) |> ignore
 #if DEBUG
 #else
     with e ->
